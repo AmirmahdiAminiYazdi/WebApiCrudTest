704996ac24f0d878214e2d1766f728086d36471f
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2023-08-18 16:08:31 +0330

    Config Project#

--- CleanArch.Domain/Models/Customer.cs
+++ CleanArch.Domain/Models/Customer.cs
@@ -2,6 +2,12 @@
 {
     public class Customer
     {
-
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string BankAccountNumber { get; set; }
     }
 }
