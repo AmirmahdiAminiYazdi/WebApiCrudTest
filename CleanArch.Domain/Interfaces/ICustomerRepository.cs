71ad5c41b3920698a18b4269c0dcea07790f9ede
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2023-08-18 17:36:49 +0330

    initial Application Layer

---
+++ CleanArch.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetCustomers();
+    }
+}
