f4bc9693db4a7c0f432b678aa14db3043fbbb800
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2023-08-18 18:17:31 +0330

    Customer Repositoy

---
+++ CleanArch.Application/Services/CustomerService.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private ICustomerRepository _customerRepository;
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public CustomerViewModel GetCustomers()
+        {
+           return new CustomerViewModel { Customers = _customerRepository.GetCustomers() };
+              
+        }
+    }
+}
---
+++ CleanArch.Infra.Data/Repository/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly CRUDContext _context;
+        public CustomerRepository(CRUDContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers;
+        }
+    }
+}
